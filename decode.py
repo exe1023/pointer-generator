18d43b7294b4b117333da6ce47053b3f5ba97faf
Lasfolt <b03902071@ntu.edu.tw>
2017-08-10 15:13:16 +0800

    modify output format of decoder

--- decode.py
+++ decode.py
@@ -57,8 +57,8 @@ class BeamSearchDecoder(object):
       # Make a descriptive decode directory name
       ckpt_name = "ckpt-" + ckpt_path.split('-')[-1] # this is something of the form "ckpt-123456"
       self._decode_dir = os.path.join(FLAGS.log_root, get_decode_dir_name(ckpt_name))
-      if os.path.exists(self._decode_dir):
-        raise Exception("single_pass decode directory %s should not already exist" % self._decode_dir)
+      #if os.path.exists(self._decode_dir):
+      #  raise Exception("single_pass decode directory %s should not already exist" % self._decode_dir)
 
     else: # Generic decode dir name
       self._decode_dir = os.path.join(FLAGS.log_root, "decode")
@@ -84,8 +84,8 @@ class BeamSearchDecoder(object):
         assert FLAGS.single_pass, "Dataset exhausted, but we are not in single_pass mode"
         tf.logging.info("Decoder has finished reading dataset for single_pass.")
         tf.logging.info("Output has been saved in %s and %s. Now starting ROUGE eval...", self._rouge_ref_dir, self._rouge_dec_dir)
-        results_dict = rouge_eval(self._rouge_ref_dir, self._rouge_dec_dir)
-        rouge_log(results_dict, self._decode_dir)
+        #results_dict = rouge_eval(self._rouge_ref_dir, self._rouge_dec_dir)
+        #rouge_log(results_dict, self._decode_dir)
         return
 
       original_article = batch.original_articles[0]  # string
@@ -142,6 +142,11 @@ class BeamSearchDecoder(object):
       sent = decoded_words[:fst_period_idx+1] # sentence up to and including the period
       decoded_words = decoded_words[fst_period_idx+1:] # everything else
       decoded_sents.append(' '.join(sent))
+    
+
+    path = os.path.join(FLAGS.log_root, 'decoded', 'decoded'+str(ex_index))
+    with open(path, 'w') as f:
+      f.write(' '.join(decoded_sents))
 
     # pyrouge calls a perl script that puts the data into HTML files.
     # Therefore we need to make our output HTML safe.
